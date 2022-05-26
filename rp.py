b13abd0b678a68e09c1e907f3bf35e6aed4913f5
jr <btunik555@seznam.cz>
2022-05-26 16:10:06 +0200

    + Checkovaní aplikací (firefox,rl,slapshot)

--- rp.py
+++ rp.py
@@ -19,6 +19,26 @@ while True:
     small_image = ""
     small_text = ""
 
+
+    if(isRunning("rocket-league")):
+        state = "Hraje si s míčkem."
+        small_image = "rl"
+        small_text = "Rocket League"
+
+    elif(isRunning("Slapshot.exe")):
+        state = "Jezdí po lede."
+        small_image = "slapshot"
+        small_text = "Slapshot Rebound"
+
+    elif (isRunning("firefox")):
+        state = "Brouzdá internetem."
+        small_image = "firefox"
+        small_text = "Mozilla Firefox"
+        details = ""
+
+    else:
+        state = "Nudí se..."
+
     RPC.update(
         state = state,
         large_image = "manjaro",
