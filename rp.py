425df1dd5783d673b39e7a5ca8dcbad0392c4c01
jr <btunik555@seznam.cz>
2022-05-27 17:51:24 +0200

    Automatické Připojování (IDE/EDITOR = FALSE)

--- rp.py
+++ rp.py
@@ -11,6 +11,7 @@ client_id = "924851988187996190"
 print(Fore.GREEN + "[+] Připojuju RPC")
 RPC = Presence(client_id=client_id)
 RPC.connect()
+connected = True
 
 
 def isRunning(app):
@@ -21,44 +22,57 @@ def isRunning(app):
 
 while True:
 
-    small_image = ""
-    small_text = ""
-
-
-    if(isRunning("RocketLeague.ex")):
-        state = "Hraje si s míčkem."
-        small_image = "rl"
-        small_text = "Rocket League"
-
-    elif(isRunning("Slapshot.exe")):
-        state = "Jezdí po lede ve Slapshotu."
-        small_image = "slapshot"
-        small_text = "Slapshot Rebound"
-
-    elif(isRunning("RobloxPlayerBet")):
-        state = "Tryhardí"
-        small_image = "roblox"
-        small_text = "Roblox"
-
-    elif (isRunning("firefox")):
-        state = "Brouzdá internetem."
-        small_image = "firefox"
-        small_text = "Mozilla Firefox"
-        details = ""
-
+    if(isRunning("pycharm.sh") or isRunning("code")):
+        RPC.close()
+        connected = False
     else:
-        state = "Nudí se..."
+        RPC.connect()
+        connected = True
 
-    RPC.update(
-        state = state,
-        large_image = "manjaro",
-        large_text = platform.release(),
-        small_image = small_image,
-        small_text = small_text
-    )
 
-    print(Fore.GREEN + "[+] Updatnuto.")
+    time.sleep(1.5)
+
+    if(connected):
+
+        small_image = ""
+        small_text = ""
+
+
+        if(isRunning("RocketLeague.ex")):
+            state = "Honí se za míčkem."
+            small_image = "rl"
+            small_text = "Rocket League"
+
+        elif(isRunning("Slapshot.exe")):
+            state = "Jezdí po lede ve Slapshotu."
+            small_image = "slapshot"
+            small_text = "Slapshot Rebound"
+
+        elif(isRunning("RobloxPlayerBet")):
+            state = "Tryhardí"
+            small_image = "roblox"
+            small_text = "Roblox"
+
+        elif (isRunning("firefox")):
+            state = "Brouzdá internetem."
+            small_image = "firefox"
+            small_text = "Mozilla Firefox"
+            details = ""
+
+        else:
+            state = "Nudí se..."
+
+        RPC.update(
+            state = state,
+            large_image = "manjaro",
+            large_text = platform.release(),
+            small_image = small_image,
+            small_text = small_text
+        )
+
+        print(Fore.GREEN + "[+] Updatnuto.")
 
     time.sleep(30)
+
 
 
